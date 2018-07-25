9e9666e48039cfdefad7e9d1a77a6368a58ec3b3
Isha <ishagar98@gmail.com>
2018-07-25 20:17:33 +0530

    qwdef

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
-import 'quake.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 
-void main () {
+void main () async {
   runApp(new MaterialApp(
     title: "Quake Report",
     home: new Quake(),
   ));
+
 }
+
+class Quake  extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Quake'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      
+    );
+  }
+
+
+}
+
+
+
